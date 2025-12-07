3dfd50c85190943a7d9e5116e4ff530095f81e95
letwir <letwirwinkuru@gmail.com>
2025-12-07 22:51:21 +0900

    fix space and add P&C

--- scripts/status.sh
+++ scripts/status.sh
@@ -2,9 +2,9 @@
 # CPUの負荷
 read F1 F2 F3 F4 F5 < /proc/loadavg
 ## １分間負荷　５分間負荷　１５分間負荷　プロセス数／待機プロセス　PID
-printf "%03s" "${F4%%/*}" # プロセス数
-printf P\["%.1f" "$F1" # -負荷
-printf "/`nproc`"\] # CPU数
+printf "　%02sP" "${F4%%/*}" # プロセス数
+printf "[%4.1f" "$F1" # -負荷
+printf "/%02s]C" $(nproc) # CPU数
 
 # I/O負荷
 INTERVAL=1
@@ -13,10 +13,9 @@ sleep $INTERVAL
 read -r _ _ _ _ _ _ _ _ _ T2 _ < /sys/class/block/mmcblk1/stat
 # 計算式：T2-T1ミリ秒を秒に、これを％にする。
 # (T2-T1) / 1000 * 100
-printf "　"%04s "$(((T2-T1)/10))% "
+printf " 　"%04s "$(((T2-T1)/10))% "
 
 # メモリ空き
 INFO=`cat /proc/meminfo |head -n2|tail -n1`
-printf "　"%06s "$((${INFO//[^0-9]/} / 1000))M"
-
+LC_NUMERIC="en_US.UTF-8" printf "　%'dM" "$((${INFO//[^0-9]/} / 1000))"
 exit 0
